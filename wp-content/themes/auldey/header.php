7ea06f49c80600285be098557041bed8ea9b3e94
v0wels <musicisdead@Chriss-MacBook-Pro.local>
2015-07-22 20:13:24 -0400

    wordpress1

--- wp-content/themes/auldey/header.php
+++ wp-content/themes/auldey/header.php
@@ -1,27 +1,67 @@
-<!-- DEVELOPER NOTE: MAKE SURE THIS IS AFTER <meta charset="utf-8" />
-                                    +        
-                                  ++++       
-                                 ++++++      
-                                ++++++++     
-                               ++++++++++    
-                              ++++++++++++   
-                              ++++++++++++,  
-                             ++++++++++++++  
-                             ++++++++++++++, 
-                            ,+++++++++++++++ 
-            +??????????????333++++++++++++++ 
-        +++++++++++++I3333333333++++++++++++ 
-      ++++++++++++733333333333333+++++++++++ 
-     +++++++++++333333333333333333++++++++++ 
-     +++++++++   333333333333333333+++++++++ 
-     ++++++++     33333333333333333+++++++++ 
-     ++++++        33333333333333333+++++++  
-     +++++          3333333333333333+++++++  
-     ++++             33333333333333++++++   
-     +++                3333333333333++++    
-      ++                  33333333333+++     
-                            333333337++      
-                               33333++       
-                                  33+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <!--
+                                        +
+                                      ++++
+                                     ++++++
+                                    ++++++++
+                                   ++++++++++
+                                  ++++++++++++
+                                  ++++++++++++,
+                                 ++++++++++++++
+                                 ++++++++++++++,
+                                ,+++++++++++++++
+                +??????????????333++++++++++++++
+            +++++++++++++I3333333333++++++++++++
+          ++++++++++++733333333333333+++++++++++
+         +++++++++++333333333333333333++++++++++
+         +++++++++   333333333333333333+++++++++
+         ++++++++     33333333333333333+++++++++
+         ++++++        33333333333333333+++++++
+         +++++          3333333333333333+++++++
+         ++++             33333333333333++++++
+         +++                3333333333333++++
+          ++                  33333333333+++
+                                333333337++
+                                   33333++
+                                      33+
 
--->
+    -->
+    <meta name="MSSmartTagsPreventParsing" content="true" /><!--[if lte IE 9]><meta http-equiv="X-UA-Compatible" content="IE=Edge"/><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <title>Title</title>
+    <link type="text/plain" rel="author" href="authors.txt" />
+    <link type="image/x-icon" rel="shortcut icon" href="favicon.ico" />
+    <?php wp_head(); ?>
+</head>
+<body>
+    <!--[if lte IE 7]><iframe src="unsupported.html" frameborder="0" scrolling="no" id="no_ie6"></iframe><![endif]-->
+
+    <header id="header">
+        <div class="header-wrap">
+            <div class="logo-container">
+                <a href="#" class="logo"><img src="ui/images/logo.png" alt="Auldey Toys" /></a>
+                <span class="red"></span>
+            </div>
+            <div id="nav-icon"><span></span></div>
+            <nav id="nav">
+                <ul>
+                    <li class="menu-item-has-children"><a href="#"><span>Brands</span></a>
+                        <ul>
+                            <li><a href="#">nav item 1</a></li>
+                            <li><a href="#">nav item 1</a></li>
+                            <li><a href="#">nav item 1</a></li>
+                            <li><a href="#">nav item 1</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><span>Videos</span></a></li>
+                    <li class="menu-item-has-children"><a href="#"><span>Support</span></a></li>
+                    <li><a href="#"><span>Our Story</span></a></li>
+                    <li><a href="#"><span>Contact</span></a></li>
+                    <li class="search"><a href="#"><span><img src="ui/images/ico-search.png" alt=""/></span></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
---
+++ wp-content/themes/auldey/page.php
@@ -0,0 +1,27 @@
+<?php get_header(); the_post(); ?>
+
+    <section id="title" style="padding-bottom:257px; margin-bottom:257px;"><!-- .noimg if no image. padding - half img height + 40 -->
+        <h2>Auldey</h2>
+        <h1>Our Story</h1>
+        <img src="http://www.placekitten.com/936/435" alt="" style="bottom:-217px; margin-left:-468px;" />
+    </section>
+
+    <section class="callout">
+        <h3>We take play seriously</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ullamcorper volutpat erat et consequat. Quisque vestibulum ut ipsum in condimentum. Nulla nunc ligula, dignissim eget laoreet eget, cursus a sem.</p>
+    </section>
+
+    <section class="main clearfix">
+        <article class="content-main">
+            <h3>Lorem ipsum dolor sit amet consectetur</h3>
+            <p>Maecenas eget tortor purus. Curabitur lacus ante, pulvinar eget ornare eu, ultricies eget nisl. Sed ornare tellus urna, vel congue mauris rutrum eget. Duis eget libero sed lectus fringilla lobortis. Maecenas velit nibh, condimentum at sapien nec, tempor tincidunt orci. Sed rhoncus lorem eu elit porttitor, pellentesque sodales velit commodo. Nam feugiat lorem ligula, sit amet dignissim diam dictum ut. </p>
+            <p>Sed ullamcorper magna ac tellus auctor commodo. Etiam lobortis feugiat ante id fringilla. Cras ut lacus diam. Vestibulum gravida, turpis in blandit vehicula, sem felis gravida mi, in egestas metus quam ut ipsum. Sed augue erat, pulvinar at congue nec, tincidunt non risus. Integer eros purus, accumsan vel sapien et, scelerisque elementum mi. Suspendisse ultrices blandit augue a consequat.</p>
+            <h4>LOREM IPSUM SUBHEAD POSTQUAM</h4>
+            <p>Etiam in lectus id massa facilisis luctus. Pellentesque eget orci metus. Aliquam auctor, massa in tincidunt ultrices, mauris quam ultricies ante, eu sagittis dui metus et urna. Duis tempus hendrerit sem a eleifend. Nam id ante ante. Nulla facilisi. </p>
+        </article>
+        <aside class="sidebar">
+            <img src="ui/images/side-superwings.jpg" alt="" />
+        </aside>
+    </section>
+
+<?php get_footer(); ?>
