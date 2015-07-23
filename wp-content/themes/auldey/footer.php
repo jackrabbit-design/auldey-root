625684dc09afc1ce6e22330f5c64e5d1480e943c
v0wels <musicisdead@Chriss-MacBook-Pro.local>
2015-07-23 17:09:38 -0400

    wordpress7

--- wp-content/themes/auldey/footer.php
+++ wp-content/themes/auldey/footer.php
@@ -1,13 +1,14 @@
     <footer id="footer" class="clearfix">
         <div class="left">
             <a href="<?php bloginfo('url'); ?>" class="logo"><img src="<?php bloginfo('url') ?>ui/images/logo-footer.png" alt="<?php bloginfo('name') ?>" /></a>
-            <p>
-                &copy; <?php echo date('Y'); ?> <?php bloginfo('name') ?> of North America. <a href="#">Terms &amp; Conditions</a> <a href="#">Coppa Policy</a> <a href="#">Privacy Policy</a>
-            </p>
+            <?php the_field('footer_text','options'); ?>
         </div>
         <div class="right">
-            <a href="#" class="social-instagram"></a>
-            <a href="#" class="social-facebook"></a>
+            <?php if($ig = get_field('instagram_username','options')){ ?>
+                <a href="http://www.instagram.com/<?php echo $ig ?>" target="_blank" class="social-instagram"></a>
+            <?php } if($fb = get_field('facebook_url','options')){ ?>
+                <a href="<?php echo $fb ?>" target="_blank" class="social-facebook"></a>
+            <?php } ?>
         </div>
     </footer>
 
